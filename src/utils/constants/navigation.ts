5ab69ed2212cb1bbd9aa814e2ec526a2391af752
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-11-18 20:08:01 -0300

    feat(web):add Header and change index.astro, NAVIGATION

--- src/utils/constants/navigation.ts
+++ src/utils/constants/navigation.ts
@@ -1,21 +1,33 @@
-const URL_HOME = '/'
-const URL_PROJECTS = '/projects'
-const URL_CONTACT = '/contact'
+const URL_HOME = '#'
+const URL_ABOUT = '#about'
+const URL_SKILLS = '#skills'
+const URL_CONTACT = '#contact'
+const URL_PROJECTS = '#projects'
 
 const NAVIGATION = [
   {
     id: 0,
-    nav: 'Home',
+    nav: 'home',
     url: URL_HOME,
   },
   {
     id: 1,
-    nav: 'Projects',
-    url: URL_PROJECTS,
+    nav: 'about',
+    url: URL_ABOUT,
   },
   {
     id: 2,
-    nav: 'Contact',
+    nav: 'projects',
+    url: URL_PROJECTS,
+  },
+  {
+    id: 3,
+    nav: 'skills',
+    url: URL_SKILLS,
+  },
+  {
+    id: 4,
+    nav: 'contact',
     url: URL_CONTACT,
   },
 ]
