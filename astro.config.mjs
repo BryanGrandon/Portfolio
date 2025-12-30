0254d0d6acde00136d799a1b8b4d70775491e1d8
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-12-30 16:16:44 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
-import preact from '@astrojs/preact';
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://BryanGrandon.github.io',
+  base: '/Portfolio',
+
   vite: {
     plugins: [tailwindcss()],
   },
